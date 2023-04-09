44f4e0489a1441b0a62e87e4209bf18b4cb87c9a
Niki <Niki@DESKTOP-APO0VJV>
2023-04-09 18:53:24 +0300

    Add project files.

---
+++ GuessANumber/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace GuessANumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNum= new Random();
+            int computerNumber=randomNum.Next(1,100);
+            int br = 0;
+            while (true)
+            {
+                Console.WriteLine("Guess a number(1-100): ");
+                string input=Console.ReadLine();
+                bool isValid=int.TryParse(input, out int playerNumber);
+                if (isValid) {
+                    if (playerNumber==computerNumber)
+                    {
+                        Console.WriteLine("You guessed it! :)");
+                        break;
+                    }
+                    else if (playerNumber>computerNumber)
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Too Low");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+                br++;
+                if (br>=5)
+                {
+                    Console.WriteLine("You don't have any other attempts.");
+                    Console.WriteLine("Sorry :(");
+                    Console.WriteLine($"The number was {computerNumber}");
+                    
+                    break;
+                }
+            }
+        }
+    }
+}
